0a71742248f65627eb6ae793fa2cc8519c9feb9c
Max75020 <maxime.duplaissy@gmail.com>
2024-03-20 10:38:31 +0100

    activite 14

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -1,3 +1,13 @@
-exports.list = (req, res) => {
-	res.send("List of woods");
-};
+const { Wood } = require("../models");
+
+exports.list = async (req, res) => {
+	try {
+		const woods = await Wood.findAll();
+		res.status(200).json(woods);
+	} catch (err) {
+		res.status(500).json({
+			message:
+				err.message || "Something wrong happened with your request to retrieve the essences of woods"
+		});
+	}
+}
