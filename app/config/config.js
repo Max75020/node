036f76c5dedf9d27535fc26763041b3d43704a60
Max75020 <maxime.duplaissy@gmail.com>
2024-03-22 17:19:14 +0100

    nrygtjedrf

--- app/config/config.js
+++ app/config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({path:"../.env"});
 module.exports =
 {
 	"development": {
@@ -6,20 +6,23 @@ module.exports =
 		"password": process.env.db_password,
 		"database": process.env.db_dev,
 		"host": process.env.db_host,
-		"dialect": process.env.db_dialect
+		"dialect": process.env.db_dialect,
+		"port": process.env.db_port
 	},
 	"test": {
 		"username": process.env.db_username,
 		"password": process.env.db_password,
 		"database": process.env.db_test,
 		"host": process.env.db_host,
-		"dialect": process.env.db_dialect
+		"dialect": process.env.db_dialect,
+		"port": process.env.db_port
 	},
 	"production": {
 		"username": process.env.db_username,
 		"password": process.env.db_password,
 		"database": process.env.db_dev,
 		"host": process.env.db_prod,
-		"dialect": process.env.db_dialect
+		"dialect": process.env.db_dialect,
+		"port": process.env.db_port
 	}
 }
