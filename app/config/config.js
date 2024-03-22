3dfcc15b55b609572e177736433f54ece7a1a519
Max75020 <maxime.duplaissy@gmail.com>
2024-03-22 18:17:18 +0100

    Update config.js

--- app/config/config.js
+++ app/config/config.js
@@ -20,8 +20,8 @@ module.exports =
 	"production": {
 		"username": process.env.db_username,
 		"password": process.env.db_password,
-		"database": process.env.db_dev,
-		"host": process.env.db_prod,
+		"database": process.env.db_prod,
+		"host": process.env.db_host,
 		"dialect": process.env.db_dialect,
 		"port": process.env.db_port
 	}
