b7e05d73db5baad8ce50abc1365c501be8d75c75
Max75020 <maxime.duplaissy@gmail.com>
2024-03-22 18:47:36 +0100

    Update auth.js

--- app/middlewares/auth.js
+++ app/middlewares/auth.js
@@ -4,7 +4,7 @@ module.exports = (req, res, next) => {
 	try {
 		const token = req.headers.authorization.split(' ')[1];
 		//decoded the token 
-		const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET);
+		const decodedToken = jwt.verify(token, process.env.token_secret);
 		const userId = decodedToken.id;
 		//save userId in req.auth var
 		req.auth = { userId };
